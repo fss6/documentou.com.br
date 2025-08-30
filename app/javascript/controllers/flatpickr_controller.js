3e58b5f89dd91c6a5a32dd0834a129040f50200c
Fábio Santos <fabiosantos.pe@gmail.com>
2025-08-30 16:47:03 -0300

    Flatpickr datetime

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,57 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    // Verificar se Flatpickr está disponível globalmente
+    if (typeof flatpickr !== 'undefined') {
+      this.initializeFlatpickr()
+    } else {
+      // Aguardar um pouco e tentar novamente
+      setTimeout(() => this.initializeFlatpickr(), 100)
+    }
+  }
+
+  initializeFlatpickr() {
+    if (typeof flatpickr === 'undefined') {
+      console.error("Flatpickr não está disponível")
+      return
+    }
+
+    // Configurações para português brasileiro
+    const config = {
+      enableTime: true,
+      dateFormat: "d/m/Y H:i",
+      time_24hr: true,
+      locale: {
+        firstDayOfWeek: 1,
+        weekdays: {
+          shorthand: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+          longhand: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
+        },
+        months: {
+          shorthand: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+          longhand: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+        },
+        rangeSeparator: " até ",
+        weekAbbreviation: "Sem",
+        amPM: ["AM", "PM"],
+        yearAriaLabel: "Ano",
+        monthAriaLabel: "Mês",
+        hourAriaLabel: "Hora",
+        minuteAriaLabel: "Minuto"
+      },
+      allowInput: true,
+      clickOpens: true,
+      minuteIncrement: 15,
+      placeholder: "Selecione data e hora"
+    }
+
+    this.flatpickr = flatpickr(this.element, config)
+  }
+
+  disconnect() {
+    if (this.flatpickr) {
+      this.flatpickr.destroy()
+    }
+  }
+}
--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,10 @@ module DocumentouComBr
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Configure timezone and locale
+    config.time_zone = "Brasilia"
+    config.i18n.default_locale = :'pt-BR'
+    config.i18n.available_locales = [:'pt-BR', :en]
+    config.i18n.fallbacks = true
   end
 end
